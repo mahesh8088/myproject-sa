3f4cef1fe0a40c23b8e6f5f5d1da12e9fc231a05
mahesh8088 <165574874+mahesh8088@users.noreply.github.com>
2024-06-06 05:48:59 +0530

    jenkins.sh

---
+++ jenkins.sh
@@ -0,0 +1,29 @@
+pipeline {
+    agent any
+    stages{
+        stage('build project'){
+            steps{
+                git url:'https://github.com/akshu20791/project-sa/', branch: "master"
+                sh 'mvn clean package'
+              
+            }
+        }
+        stage('Build docker image'){
+            steps{
+                script{
+                    sh 'docker build -t mahesh8088/staragileprojectfinance:v1 .'
+                    sh 'docker images'
+                }
+            }
+        }
+         
+        
+     stage('Deploy') {
+            steps {
+                sh 'sudo docker run -itd --name My-first-containe21211 -p 8083:8081 mahesh8088/staragileprojectfinance:v1'
+                  
+                }
+            }
+        
+    }
+}
